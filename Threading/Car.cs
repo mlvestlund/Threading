a42bdfa95b6e9e33cd07a5605be03f0694136613
mlvestlund <mariavestlund@yahoo.se>
2023-12-17 22:21:15 +0100

    Add project files.

---
+++ Threading/Car.cs
@@ -0,0 +1,75 @@
+﻿namespace Threading
+{
+    internal class Car
+    {
+        Random random = new Random();
+        public string Name { get; }
+        public double Distance { get; private set; }
+        public int Speed { get; private set; }
+
+        private int RunRandomCarIssue = 0;
+
+        public static string winner = "";
+
+        public Car(string name)
+        {
+            Name = name;
+            Distance = 0;
+            Speed = 120;
+        }
+
+        public async Task RaceAsync()
+        {
+            Console.WriteLine($"{Name} starts!");
+            while (Distance < 10)
+            {
+                await Task.Delay(1000);
+                Distance += Speed / 3600.0;
+                if (RunRandomCarIssue % 30 == 0)
+                {
+                    RandomCarIssues();
+                }
+
+                RunRandomCarIssue++;
+            }
+
+            if (Distance >= 10)
+            {
+                Console.WriteLine($"{Name} crossed the finish line!");
+                string currentWinner = Name;
+                Interlocked.CompareExchange(ref winner, currentWinner, "");
+
+                if(Name == winner)
+                {
+                    Console.WriteLine($"{winner} won the race!");
+                }
+            }
+        }
+
+        public void RandomCarIssues()
+        {
+            int chance = random.Next(50);
+
+            if (chance == 0)
+            {
+                Console.WriteLine($"{Name} stops for refueling for 30 sec.");
+                Thread.Sleep(30000);
+            }
+            else if (chance <= 2)
+            {
+                Console.WriteLine($"{Name} has tire puncture and stops to change tires for 20 sec.");
+                Thread.Sleep(20000);
+            }
+            else if (chance <= 7)
+            {
+                Console.WriteLine($"{Name} got a bird in its windshield and stops to clean it for 10 sec.");
+                Thread.Sleep(10000);
+            }
+            else if (chance <= 17)
+            {
+                Console.WriteLine($"{Name} has angine failure and speed declines with 1 km/h.");
+                Speed -= 1;
+            }
+        }
+    }
+}
---
+++ Threading/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Threading
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Car competitor1 = new Car("Volvo");
+            Car competitor2 = new Car("Citroen");
+
+            Console.WriteLine("Ready, set, GO!");
+
+            Task race1 = Task.Run(competitor1.RaceAsync);
+            Task race2 = Task.Run(competitor2.RaceAsync);
+
+            while (!race1.IsCompleted || !race2.IsCompleted)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true);
+
+                    if (keyInfo.Key == ConsoleKey.Enter)
+                    {
+                        Console.WriteLine("\nStatus:");
+                        Console.WriteLine($"{competitor1.Name}: {competitor1.Distance:0.#} km, {competitor1.Speed} km/h");
+                        Console.WriteLine($"{competitor2.Name}: {competitor2.Distance:0.#} km, {competitor2.Speed} km/h");
+                    }
+                }
+            }
+
+            await Task.WhenAll(race1, race2);
+            Console.ReadLine();
+        }
+    }
+}
